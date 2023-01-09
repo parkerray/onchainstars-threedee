6cefe719b2a73c15be277e5e83d6400dba5e2afd
parkerray <parker.thompson@webflow.com>
2023-01-09 11:21:09 -0600

    paths test

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 echo > .nojekyll
 
 # if you are deploying to a custom domain
-echo 'www.parkerscode.com' > CNAME
+echo 'www.parkerscode.com/onchainstars-threedee' > CNAME
 
 git init
 git checkout -B main
